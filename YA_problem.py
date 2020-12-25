dca21df86d74bba4f3da4847b5fe8f053947739d
Denis Godunov <den.godunov@gmail.com>
2020-12-25 15:32:54 +0300

    Update YA_problem.py

--- YA_problem.py
+++ YA_problem.py
@@ -1,11 +1,17 @@
 def num_of_sets(l):
-    distinct_sweets = set(l)
-    dict_of = {}
+    """
+    num_of_sets(list of strings) -> int
+    num_of_sets(['a']) -> 1
+    """
+    distinct_sweets = set(l) #let's find all distinct sweets from input list
+    dict_of = {} #empty dict to store key:value (sweet:number of occurrences)
+
     for i in distinct_sweets:
         dict_of[i] = l.count(i)
+        
     key_min = min(dict_of.keys(), key=(lambda k: dict_of[k]))
-    print(dict_of[key_min])
-
-num_of_sets(['a', 'b', 'c'])
-num_of_sets(['a', 'b', 'c', 'a', 'b', 'c','d'])
-num_of_sets(['a', 'b', 'c', 'a', 'b', 'c'])
+    return dict_of[key_min]
+    
+assert num_of_sets(['a', 'b', 'c']) == 1
+assert num_of_sets(['a', 'b', 'c', 'a', 'b', 'c','d']) == 1
+assert num_of_sets(['a', 'b', 'c', 'a', 'b', 'c']) == 2
